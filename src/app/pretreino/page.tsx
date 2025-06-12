cb68f8b8c23d3df10f2407bbe006cfa146b55528
iPonzu <joao_ignaciocastro1603@hotmail.com>
2025-06-12 20:03:45 -0300

    :sparkles: Reestilizando o background dos quadros

--- src/app/pretreino/page.tsx
+++ src/app/pretreino/page.tsx
@@ -7,7 +7,6 @@ export default function Page() {
   return (
     <div className="relative min-h-screen bg-black flex flex-col">
       <_menu />
-
       <div className="flex flex-1 items-center justify-center px-4 py-12">
         <div className="flex flex-col lg:flex-row items-center justify-center gap-10 max-w-6xl w-full">
           <div className="w-full lg:w-1/2 flex justify-center">
@@ -19,7 +18,7 @@ export default function Page() {
               className="max-w-xs sm:max-w-sm md:max-w-md h-auto"
             />
           </div>
-          <div className="w-full lg:w-1/2 text-white border border-white p-6 rounded-2xl backdrop-blur-sm bg-white/10">
+          <div className="w-full lg:w-1/2 text-white border bg-purple-950 border-white p-6 backdrop-blur-sm">
             <h1 className="text-2xl sm:text-3xl font-bold mb-4">Pré Treino Energyze</h1>
             <p className="text-sm sm:text-base mb-6">
               O Pré-Treino Energyze é um suplemento potente desenvolvido para aumentar a energia,
@@ -28,7 +27,7 @@ export default function Page() {
             </p>
             <div className="flex flex-wrap gap-4">
               <button
-                className="bg-transparent text-white font-bold py-2 px-4 border border-white rounded-md hover:bg-purple-700 transition"
+                className="bg-transparent text-white font-bold py-2 px-4 border border-white hover:bg-purple-600 transition"
                 onClick={() =>
                   window.open(
                     "https://produto.mercadolivre.com.br/MLB-4045897809-energyze-pre-treino-pink-lemonade-pote-240g-_JM",
@@ -39,7 +38,7 @@ export default function Page() {
                 Pink Lemonade
               </button>
               <button
-                className="bg-transparent text-white font-bold py-2 px-4 border border-white rounded-md hover:bg-orange-400 transition"
+                className="bg-transparent text-white font-bold py-2 px-4 border border-white hover:bg-orange-400 transition"
                 onClick={() =>
                   window.open(
                     "https://produto.mercadolivre.com.br/MLB-4046100507-energyze-pre-treino-tangerina-pote-240g-_JM",
